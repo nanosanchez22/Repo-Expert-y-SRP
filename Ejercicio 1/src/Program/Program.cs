75dd00bdb70c52de94d364ac8e20028c9445bac0
nanosanchez22 <nanosanchez22@gmail.com>
2023-08-28 12:00:44 -0300

    crear clase sector y shelf, y corregir program

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -7,11 +7,35 @@ namespace SRP
     {
         static void Main()
         {
-            Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
-            Book book2 = new Book("Pro C#","Troelsen","001-035");
-            book1.ShelveBook("A","7");
-            book2.ShelveBook("B","3");
+            
 
+            Book book1 = new Book("Design Patterns", "Erich Gamma & Others", "001-034");
+            Book book2 = new Book("Pro C#", "Troelsen", "001-035");
+
+            Shelf shelfA = new Shelf();
+            shelfA.ShelfName = "shelf a";
+            Shelf shelfB = new Shelf();
+            shelfB.ShelfName = "shelf b";
+
+            Sector sectorA = new Sector();
+            sectorA.SectorName = "sector a";
+
+            shelfA.Books.Add(book1);
+            shelfB.Books.Add(book2);
+
+            sectorA.Shelf.Add(shelfA);
+            sectorA.Shelf.Add(shelfB);
+
+            Console.WriteLine($"Sector: {sectorA.SectorName}");
+            foreach (var shelf in sectorA.Shelf)
+            {
+                Console.WriteLine($"  Shelf: {shelf.ShelfName}");
+                foreach (var book in shelf.Books)
+                {
+                    Console.WriteLine($"    Book: {book.Title} by {book.Author}, Code: {book.Code}");
+                }
+            }
+        
         }
     }
 }
